a2321c383b0ba7a48103b89471ef47c594d27a12
Sujoy ॐ <iamsujoyp@gmail.com>
2025-05-29 22:55:13 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @SHIVANSH474 × @MASTIWITHFRIENDSXD •"
+            message, f"```{str(ac)}```\n• @UncleChipssBot × @SuperToppers •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"

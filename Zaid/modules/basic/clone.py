ed1dd006c24a7d3db93846149ab779a0dafca9e9
Sujoy ॐ <iamsujoyp@gmail.com>
2025-05-29 22:39:35 +0530

    Update clone.py

--- Zaid/modules/basic/clone.py
+++ Zaid/modules/basic/clone.py
@@ -8,8 +8,8 @@ from Zaid.helper.basic import edit_or_reply, get_text, get_user
 
 from Zaid.modules.help import *
 
-OWNER = os.environ.get("OWNER", "⊷𓆩🇸​𝗧𝗥𝗔𝗡𝗚𝗘𝗥•─‌⃛≛⃝🦅 ༆𝐗𝐃𐏓")
-BIO = os.environ.get("BIO", "💕ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ sᴛʀᴀɴɢᴇʀ💕")
+OWNER = os.environ.get("OWNER", "—͟͟͞͞𝙎𝙪𝙟𝙤𝙮™ </>")
+BIO = os.environ.get("BIO", "💕ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ Sᴜᴘᴇʀ_Tᴏᴘᴘᴇʀs💕")
 
 
 @Client.on_message(filters.command("clone", ".") & filters.me)

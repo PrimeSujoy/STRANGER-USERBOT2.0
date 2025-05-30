b6a2240b4ba05fccf7b197ac083df22462bb1762
Sujoy ॐ <iamsujoyp@gmail.com>
2025-05-30 16:02:39 +0530

    Update carbon.py

--- Zaid/modules/basic/carbon.py
+++ Zaid/modules/basic/carbon.py
@@ -4,17 +4,17 @@ from io import BytesIO
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Zaid import aiosession
+import aiohttp
 
 from Zaid.helper.PyroHelpers import ReplyCheck
-
 from Zaid.modules.help import add_command_help
 
 
-async def make_carbon(code):
+async def make_carbon(code: str) -> BytesIO:
     url = "https://carbonara.vercel.app/api/cook"
-    async with aiosession.post(url, json={"code": code}) as resp:
-        image = BytesIO(await resp.read())
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json={"code": code}) as resp:
+            image = BytesIO(await resp.read())
     image.name = "carbon.png"
     return image
 
@@ -23,16 +23,14 @@ async def make_carbon(code):
 async def carbon_func(client: Client, message: Message):
     text = (
         message.text.split(None, 1)[1]
-        if len(
-            message.command,
-        )
-        != 1
+        if len(message.command) != 1
         else None
     )
     if message.reply_to_message:
         text = message.reply_to_message.text or message.reply_to_message.caption
     if not text:
         return await message.delete()
+
     ex = await message.edit_text("`Preparing Carbon . . .`")
     carbon = await make_carbon(text)
     await ex.edit("`Uploading . . .`")

c5101fd8972f23dd93d267738e19fbd71cd6d9c2
Sujoy ॐ <iamsujoyp@gmail.com>
2025-06-02 21:22:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/itzshukla/STRANGER-USERBOT2.0")
+REPO_URL = getenv("REPO_URL", "https://github.com")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "1BVtsOKIBu3Jh5-_0ETAZE-TbtkSPIZ2xQBGhIXgc0BVMOEm8K-Okhn4HGMxFhefzBev07vjaPOmW3Sp0AbmvdRbqhmxrR4EPJkAMBtkOZ5Ss1_SoU5_b8RYJb4W4Nc_fUN_fbwDvKdg4mo_KN0o_Yg90xaEPSwEuCmQUeQbRVFdgoI5tOnGeKqbIipR3O5ahd0vykXm5MHPH9jTHyhMjDlakUoGugTUDl5GQlHesj2dKHU2d-g1QgvDduOTWh4gMEo6DrrAbdhmi0k3l4iz1A4PA6Li4OG0ZL-Kkv7a6Ke2TExPY9zHIZ811knfixiwchEbkdBDOvmkSz-FgTBUbnEly1l70LDA=")
